f4b63a78df30854426c22ac179c98ccb56318f7e
PotassiumFl <3390945494@qq.com>
2025-02-20 20:56:59 +0800

    2/20 upload

---
+++ lab/lab1/Student.java
@@ -0,0 +1,17 @@
+public class Student {
+    String name;
+    int score;
+
+    public Student() {
+
+    }
+
+    public Student(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public int getScore() {
+        return this.score;
+    }
+}
--- lab/lab1/lab1.java
+++ lab/lab1/lab1.java
@@ -1,15 +1,42 @@
 import java.io.*;
+import java.util.*;
 
 public class lab1 {
     public static void main(String[] args) {
-        File file1 = new File("F:\\vscode\\oop\\lab\\lab1\\grade.txt");
-        if (file1.isFile() && file1.exists()) {
-            System.err.println("1");
-        } else {
-            System.err.println("0");
+        System.setProperty("sun.stdout.encoding", "UTF-8");
+        File file = new File("F:\\vscode\\oop\\lab\\lab1\\grade.txt");
+        try {
+            int count = 0;
+            int sum = 0;
+            int average = 0;
+            String bufferString;
+            List<Student> studentList = new ArrayList<>();
+            FileReader fileReader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            while ((bufferString = bufferedReader.readLine()) != null) {
+                String[] clip = bufferString.split(" ");
+                studentList.add(new Student(clip[0], Integer.parseInt(clip[1])));
+                count++;
+            }
+
+            for (int i = 0; i < count - 1; i++) {
+                sum += studentList.get(i).score;
+            }
+
+            average = sum / (count - 1);
+            System.out.println("平均成绩是：" + average);
+
+            studentList.sort(Comparator.comparingInt(Student::getScore).reversed());
+
+            for (int i = 0; i < count - 1; i++) {
+                System.err.println(studentList.get(i).name);
+            }
+
+            bufferedReader.close();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        // FileReader fileReader = new FileReader(file1);
-        // BufferedReader bufferedReader = new BufferedReader(fileReader);
-        // System.err.println(System.getProperty("user.dir"));
+
     }
 }
