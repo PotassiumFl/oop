e5b0a49a3deec30170075f45e69439746c555aa5
PotassiumFl <3390945494@qq.com>
2025-02-20 19:17:38 +0800

    2/20 upload

--- lab/lab1/lab1.java
+++ lab/lab1/lab1.java
@@ -1,17 +1,15 @@
-package lab.lab1;
-
 import java.io.*;
 
 public class lab1 {
     public static void main(String[] args) {
-        File file1 = new File("../grade.txt");
-        if (file1.exists()) {
+        File file1 = new File("F:\\vscode\\oop\\lab\\lab1\\grade.txt");
+        if (file1.isFile() && file1.exists()) {
             System.err.println("1");
         } else {
             System.err.println("0");
         }
         // FileReader fileReader = new FileReader(file1);
         // BufferedReader bufferedReader = new BufferedReader(fileReader);
-
+        // System.err.println(System.getProperty("user.dir"));
     }
 }
