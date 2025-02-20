f5b89775fce91896a83bdfd927184344b901593f
PotassiumFl <3390945494@qq.com>
2025-02-20 17:00:54 +0800

    2/20 upload

--- lab/lab1/lab1.java
+++ lab/lab1/lab1.java
@@ -1,7 +1,12 @@
 package lab.lab1;
 
+import java.io.*;
+
 public class lab1 {
     public static void main(String[] args) {
+        File file = new File(".\\grade.txt");
+        FileReader fileReader = new FileReader(file);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
 
     }
 }
